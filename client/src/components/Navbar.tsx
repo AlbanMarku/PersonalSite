ce67c1f604d3d0778ab7ce4c47bacc579f29e6ff
AlbanMarku <albanmarku@outlook.com>
2022-12-23 21:44:42 +0000

    Nav logic.

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,20 +1,29 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/navbar.css';
 
 function Navbar() {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const handleExpand = () => {
+    setIsExpanded(!isExpanded);
+  };
+
   return (
     <nav className="navigation">
       <a href="/" className="brand-name">
         My Site
       </a>
-      <button type="button" className="hamburger">
+      <button onClick={handleExpand} type="button" className="hamburger">
         <svg viewBox="0 0 100 80" width="40" height="40">
           <rect width="100" height="20" rx="10" />
           <rect y="30" width="100" height="20" rx="10" />
           <rect y="60" width="100" height="20" rx="10" />
         </svg>
       </button>
-      <div className="navigation-menu">
+      <div
+        className={isExpanded ? 'navigation-menu expanded' : 'navigation-menu'}
+      >
         <ul>
           <li>
             <Link className="link" to="/">
