25c4a9d6dd69a92824daee7cbaf9ff8a89316d26
AlbanMarku <albanmarku@outlook.com>
2022-12-29 16:06:49 +0000

    Basic home layout.

--- client/src/views/Home.tsx
+++ client/src/views/Home.tsx
@@ -1,9 +1,18 @@
 import '../styles/home.css';
+import Background from '../assets/background.jpg';
 
 function Home() {
   return (
     <div className="Home">
-      <h4>Home</h4>
+      <div
+        className="welcomeSection"
+        style={{ backgroundImage: `url(${Background})` }}
+      >
+        <h1>Welcome to my page</h1>
+      </div>
+      <div className="aboutMe">
+        <h1>this is stuff about me</h1>
+      </div>
     </div>
   );
 }
