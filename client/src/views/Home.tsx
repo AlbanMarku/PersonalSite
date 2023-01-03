a839941923d2cdd003ef35325d002b076c198f6c
AlbanMarku <albanmarku@outlook.com>
2023-01-03 16:30:39 +0000

    Basic icon setup.

--- client/src/views/Home.tsx
+++ client/src/views/Home.tsx
@@ -1,6 +1,17 @@
 import '../styles/home.css';
 import logo from '../assets/logo.png';
-import htmlLogo from '../assets/HTML5.svg';
+import htmlLogo from '../assets/html5Logo.svg';
+import csslogo from '../assets/cssLogo.svg';
+import expresslogo from '../assets/expressLogo.svg';
+import jslogo from '../assets/jsLogo.svg';
+import mongologo from '../assets/mongodbLogo.svg';
+import nodejslogo from '../assets/nodejsLogo.svg';
+import npmlogo from '../assets/npmLogo.svg';
+import reactlogo from '../assets/reactLogo.svg';
+import tslogo from '../assets/typescriptLogo.svg';
+import webpacklogo from '../assets/webpackLogo.svg';
+import vitestLogo from '../assets/vitestLogo.svg';
+import gitLogo from '../assets/githubLogo.svg';
 
 function Home() {
   return (
@@ -24,7 +35,42 @@ function Home() {
         </p>
         <p>Contact me for web development opportunities.</p>
         <div className="icons">
-          <img className="logos" src={htmlLogo} alt="html5" />
+          <div className="logos">
+            <img src={htmlLogo} alt="html5" />
+          </div>
+          <div className="logos">
+            <img src={csslogo} alt="html5" />
+          </div>
+          <div className="logos">
+            <img src={expresslogo} alt="html5" />
+          </div>
+          <div className="logos">
+            <img src={jslogo} alt="html5" />
+          </div>
+          <div className="logos">
+            <img src={mongologo} alt="html5" />
+          </div>
+          <div className="logos">
+            <img src={nodejslogo} alt="html5" />
+          </div>
+          <div className="logos">
+            <img src={npmlogo} alt="html5" />
+          </div>
+          <div className="logos">
+            <img src={reactlogo} alt="html5" />
+          </div>
+          <div className="logos">
+            <img src={tslogo} alt="html5" />
+          </div>
+          <div className="logos">
+            <img src={webpacklogo} alt="html5" />
+          </div>
+          <div className="logos">
+            <img src={vitestLogo} alt="html5" />
+          </div>
+          <div className="logos">
+            <img src={gitLogo} alt="html5" />
+          </div>
         </div>
       </div>
       <div className="projectSection">
