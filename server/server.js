67d04ddda6c5904212625ebf108bb1f0c7172187
AlbanMarku <albanmarku@outlook.com>
2023-01-07 18:00:20 +0000

    Server side email function.

--- server/server.js
+++ server/server.js
@@ -16,4 +16,31 @@ app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../client/dist', 'index.html'));
 });
 
-app.post('/sendMail', async (req, res) => {});
+app.post('/sendMail', async (req, res) => {
+  try {
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: 'meritakol@gmail.com',
+        pass: 'rqrhespohaydmmol',
+      },
+    });
+
+    const info = {
+      from: '"personal site" <no-reply@example.com>',
+      subject: 'CV response',
+      to: 'meritakol@gmail.com',
+      text: `You got a response from ${req.body.email}:\n${req.body.message}`,
+    };
+
+    transporter.sendMail(info, (err, feedback) => {
+      if (err) console.log(err);
+      else console.log(feedback);
+    });
+
+    console.log('done');
+    res.send('Email sent');
+  } catch (error) {
+    console.log(error);
+  }
+});
