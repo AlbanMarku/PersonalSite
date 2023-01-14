658640af94b807c152e45d43357aae5aecefc184
AlbanMarku <albanmarku@outlook.com>
2023-01-14 13:17:23 +0000

    Env vars.

--- server/server.js
+++ server/server.js
@@ -7,6 +7,9 @@ const app = express();
 const port = process.env.PORT || 8080;
 app.listen(port);
 
+const { EMAIL } = process.env;
+const { PASSWORD } = process.env;
+
 app.use(express.static(path.resolve(__dirname, '../client/dist')));
 app.use(express.json({ limit: '1mb', strict: true }));
 
@@ -20,8 +23,8 @@ app.post('/sendMail', async (req, res) => {
   const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-      user: 'meritakol@gmail.com',
-      pass: 'yhnbutcpzrmpjbdh',
+      user: EMAIL,
+      pass: PASSWORD,
     },
   });
 
@@ -29,7 +32,7 @@ app.post('/sendMail', async (req, res) => {
     const info = await transporter.sendMail({
       from: '"personal site" <no-reply@example.com>',
       subject: 'CV response',
-      to: 'meritakol@gmail.com',
+      to: EMAIL,
       text: `You got a response from ${req.body.email}:\n${req.body.message}`,
     });
 
